c35f67e8499103037a9697ddb12aedd65c65608a
JawaharPaisal <jawaharpaisal@gmail.com>
2024-12-18 14:25:40 +0530

    2

--- routes/authroute.js
+++ routes/authroute.js
@@ -1,4 +1,4 @@
-const { Signup, Login } = require('../Controllers/AuthController')
+const { Signup, Login } = require('../controllers/AuthController')
 
 const {userVerification} = require('../middlewares/authmiddleware')
 const router = require('express').Router()
